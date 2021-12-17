9adae874d7abbecf1fa5be29d0a61d7d81530331
William <williamollio98@gmail.com>
2021-12-17 20:53:50 +0100

    deleted folders

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -3,14 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
+/*   By: wollio <williamollio@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 13:39:09 by wollio            #+#    #+#             */
-/*   Updated: 2021/12/17 15:58:35 by wollio           ###   ########.fr       */
+/*   Updated: 2021/12/17 20:35:28 by wollio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 // :: scope resolution operator
 // << insert operator
@@ -19,7 +17,7 @@
 // namespace::function or variable
 // flush save data from a temporary buffer to permanent storage
 // end : \n + flush the stream
-// stdout/count is line buffered that is the output doesnt get send to the OS until a newline is writte or the buffer is flushed
+// stdout/count is line buffered that is the output doesnt get send to the OS until a newline is written or the buffer is flushed
 // std::cin (char input): read input from a buffer stream + if anything is pending for output, it's got flushed
 
 // constructor and destructor haven't returned value
