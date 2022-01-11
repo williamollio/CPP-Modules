f48f790d0b15065e6fd74fbce38d626bc389091a
William Ollio <williamollio98@gmail.com>
2022-01-11 19:15:44 +0100

    split working

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -3,25 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wollio <williamollio@student.42.fr>        +#+  +:+       +#+        */
+/*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 16:23:14 by wollio            #+#    #+#             */
-/*   Updated: 2022/01/05 22:30:44 by wollio           ###   ########.fr       */
+/*   Updated: 2022/01/11 18:22:44 by wollio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string.h>
-#include <fstream>
-#include <sstream>
+#include "replace.hpp"
 
 using namespace std;
+
+int ft_count_occ(string s, const char *del, string s1)
+{
+	int count = 0;
+	size_t pos = s.find(s1, 0);
+	while (pos != -1)
+	{
+		count++;
+		pos = s.find(s1, pos + 1);
+	}
+	return (count);
+}
+
+int ft_count_words(string s)
+{
+	int count = 1;
+	stringstream stream(s);
+	string oneWord;
+	while (stream >> oneWord)
+		count++;
+	return (count);
+}
 
 int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
 		cout << "Error number of arguments" << endl;
+		cout << "Usage : \"path to file.txt\" \"string1\" \"string2\"" << endl;
 		return (EXIT_FAILURE);
 	}
 	fstream file;
@@ -36,13 +56,32 @@ int main(int argc, char *argv[])
 
 	stringstream buffer;
 	buffer << file.rdbuf();
-	cout << "s : " << buffer.str() << endl;
-	cout << "s1 : " << s1 << endl;
-	cout << "s2 : " << s2 << endl;
-	// find returns npos if the character isn't found
-	//string del = " ";
-	//while ()
+	string s = buffer.str();
 
+	const char *del = " ";
+	char *s_modif = const_cast <char *>(s.c_str());
+	int count_occ = ft_count_occ(s, del, s1);
+	char *ptr = strtok(s_modif, del);
+	int count_words = ft_count_words(s);
+	char *arr[count_words];
+	int i = 0;
+	while (ptr != NULL)
+	{
+		stringstream stream;
+		string ptr_string;
+		stream << ptr;
+		stream >> ptr_string;
+		if (s1 == ptr_string)
+			arr[i] = const_cast <char *>(s2.c_str());
+		else
+			arr[i] = ptr;
+		ptr = strtok(NULL, del);
+		i++;
+	}
+	for (int i = 0; i < count_words ; i++)
+	{
+		cout << "arr[i]" << arr[i] << endl;
+	}
 	file.close();
 	// int dst;
 	// int dst2;
---
+++ C01/ex04/replace.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 14:22:13 by wollio            #+#    #+#             */
+/*   Updated: 2022/01/11 16:40:02 by wollio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_H
+# define REPLACE_H
+
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+class Replace {
+
+	private:
+
+	public:
+	Replace(void);
+	~Replace(void);
+};
+
+#endif
