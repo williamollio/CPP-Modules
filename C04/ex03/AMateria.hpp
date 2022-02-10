ce792b92b121cdb47292a8f3241549224e6c6b74
William <williamollio98@gmail.com>
2022-02-10 19:51:58 +0100

    init ex03

---
+++ C04/ex03/AMateria.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiliamollio <wiliamollio@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 18:08:59 by wiliamollio       #+#    #+#             */
+/*   Updated: 2022/02/10 18:10:57 by wiliamollio      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_H
+# define AMATERIA_H
+
+#include <iostream>
+#include <cstring>
+
+# define PRINT(__msg) (std::cout << __msg << std::endl)
+
+class AMateria
+{
+	protected:
+	public:
+		AMateria(std::string const & type);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
