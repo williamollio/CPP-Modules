7570c5f20c07cab7a19a9c908735032986159254
William Ollio <williamollio98@gmail.com>
2022-02-16 17:16:00 +0100

    ex01 done

---
+++ C05/ex01/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 17:51:25 by wollio            #+#    #+#             */
+/*   Updated: 2022/02/16 16:16:29 by wollio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <cstring>
+# include <exception>
+# include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+	public:
+		Bureaucrat(void);
+		Bureaucrat(std::string name, int grade);
+		~Bureaucrat(void);
+		Bureaucrat(const Bureaucrat &copy);
+		std::string getName() const;
+		int getGrade() const;
+		void increment();
+		void decrement();
+		void signForm(Form form);
+		Bureaucrat	&operator = (const Bureaucrat &copy);
+	class GradeTooHighException : public std::exception
+	{
+		const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		const char* what() const throw();
+	};
+};
+std::ostream & operator<<(std::ostream &out, const Bureaucrat &c);
+
+#endif
---
+++ C05/ex01/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 16:09:39 by wollio            #+#    #+#             */
+/*   Updated: 2022/02/16 17:04:57 by wollio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	std::cout << "try to create a form with wrong grades\n";
+	Bureaucrat newbie = Bureaucrat("new", 50);
+	try {
+		Form formula = Form("work", 0, 50);
+	}
+	catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << "\ncreating a valid form an sign it\n";
+	Form form = Form("workload", 90, 90);
+	std::cout << form << std::endl;
+	try {
+		form.beSigned(newbie);
+	}
+	catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << form << std::endl;
+
+	std::cout << "\ntry to sign a form with a to high grade\n";
+	std::cout << newbie << std::endl;
+	Form president = Form("top secret", 1, 1);
+	std::cout << president << std::endl;
+	try {
+		president.beSigned(newbie);
+	}
+	catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << president << std::endl;
+	return (0);
+}
