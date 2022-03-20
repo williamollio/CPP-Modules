efb001cdcd7546050dc4e5bf4d96409b35dcf27f
William Ollio <williamollio98@gmail.com>
2022-03-21 00:10:16 +0100

    ex00 fix include

--- C07/ex00/Whatever.hpp
+++ C07/ex00/Whatever.hpp
@@ -1,3 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 00:04:20 by wollio            #+#    #+#             */
+/*   Updated: 2022/03/21 00:05:46 by wollio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
 template<typename T>
 void swap(T &arg, T &arg2)
 {
@@ -22,3 +38,5 @@ T const &max(T const &arg, T const &arg2)
 		return arg;
 	return arg2;
 }
+
+#endif
--- C07/ex00/main.cpp
+++ C07/ex00/main.cpp
@@ -6,22 +6,21 @@
 /*   By: wollio <wollio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 12:52:32 by wollio            #+#    #+#             */
-/*   Updated: 2022/03/15 15:37:14 by wollio           ###   ########.fr       */
+/*   Updated: 2022/03/21 00:09:53 by wollio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Whatever.hpp"
-#include <iostream>
 
 int main(void)
 {
-	int a = 1;
-	int b = 2;
-	::swap(a, b);
+	int a = -1;
+	int b = 10;
 
 	std::cout << "a : " << a << std::endl
-	<< "b : " << b << std::endl
-	<< "swap " << std::endl
+	<< "b : " << b << std::endl;
+	::swap(a, b);
+	std::cout << "swap " << std::endl
 	<< "a : " << a << std::endl
 	<< "b : " << b << std::endl
 	<< "min : " << ::min(a, b) << " | max : " << ::max(a, b) << std::endl;
